1c70dc467ec90adc061bdddf3a16682b83052876
Noah2211 <noah.decroix3@gmail.com>
2022-01-21 10:19:10 +0100

    done

--- index.php
+++ index.php
@@ -11,7 +11,36 @@ require 'form.php';
     <title>Home</title>
 </head>
 <body>
-<form action="index.php?f=0" method="post">
+
+
+
+<?php
+if (isset($_POST['submit'])) { ?>
+        <div><?= $_POST['user_civilite'] ?></div>
+        <div><?= $_POST['user_name']?></div>
+        <div><?= $_POST['user_fam_name']?></div><?php
+    if (isset($_FILES['user_file']) && $_FILES['user_file']['error'] === 0) {
+        $info = pathinfo($_FILES['user_file']['name']);
+        $autorisedExtension = ['pdf'];
+
+        foreach ($autorisedExtension as $value) {
+            if ($info['extension'] === $value) {
+                echo "L'extension du fichier est ok, le fichier a bien été envoyer";
+            }
+            else {
+                echo "Ce n'est pas la bonne extension vous devez envoyer un pdf";
+            }
+        }
+    }
+}
+elseif (isset($_GET['submit'])) { ?>
+        <div><?= $_GET['user_civilite'] ?></div>
+        <div><?= $_GET['user_name']?></div>
+        <div><?= $_GET['user_fam_name']?></div><?php
+}
+
+else {?>
+<form action="index.php?f=0" method="post" enctype="multipart/form-data">
     <label for="id-civil">Civilité: </label>
     <select name="user_civilite" id="id-civil">
         <option value="Mr">Mr</option>
@@ -24,7 +53,12 @@ require 'form.php';
     <label for="id-fam-name">Nom</label>
     <input type="text" id="id-fam-name" name="user_fam_name">
 
+    <input type="file" name="user_file">
+
     <input type="submit" name="submit">
-</form>
+</form><?php
+}
+
+?>
 </body>
 </html>
